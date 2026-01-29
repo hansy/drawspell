254fd34f57c3bb121faebce5dbc5462003c30d90
Hans Yadav <hansy@users.noreply.github.com>
2026-01-29 05:50:57 -0500

    add posthog to server

--- apps/server/src/analytics/posthog.ts
+++ apps/server/src/analytics/posthog.ts
@@ -1,7 +1,7 @@
 import { PostHog } from "posthog-node";
 
 export const createPostHogClient = (env: Env) => {
-  const posthog = new PostHog(env.POSTHOG_API_KEY, {
+  const posthog = new PostHog(env.POSTHOG_API_KEY ?? "", {
     host: env.POSTHOG_API_HOST,
     flushAt: 1, // Send events immediately in edge environment
     flushInterval: 0, // Don't wait for interval
